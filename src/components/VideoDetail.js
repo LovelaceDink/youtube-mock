31edbebe17d6500696320a2e1858526346d0ad86
Wayne Wilcox <wilcox_wayne@hotmail.com>
2021-05-28 14:55:26 -0400

    Update VideoDetail.js

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 const videoDetail = ({ video }) => {
   if (!video) return <div>Let's look for some videos...</div>;
 
+  // some videos have restricted access, but that's ok for this toy app.
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   const videoTitle = video.snippet.title;
   const videoDescription = video.snippet.description;
